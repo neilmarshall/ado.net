7e2dc04a9739d8e5f6b6f0b89721c783b2c8b567
neilmarshall <neil.marshall@dunelm.org.uk>
2020-11-11 21:35:28 +0000

    initial commit

---
+++ ADO.Net.Examples/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Configuration;
+using System.Data.SqlClient;
+
+namespace ADO.Net.Examples
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var connectionString = ConfigurationManager.ConnectionStrings["AzureDBConnection"].ConnectionString;
+            using var conn = new SqlConnection(connectionString);
+            using var cmd = new SqlCommand("SELECT COUNT(*) FROM production.products;", conn);
+            conn.Open();
+            var count = (int)cmd.ExecuteScalar();
+            Console.WriteLine(count);
+        }
+    }
+}
