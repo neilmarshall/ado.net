0b217cfd520d9780733f7d72f80916e99f3f86e2
neilmarshall <neil.marshall@dunelm.org.uk>
2020-11-11 21:44:19 +0000

    add demos of ExecuteScalar[Async] methods

--- ADO.Net.Examples/Program.cs
+++ ADO.Net.Examples/Program.cs
@@ -1,18 +1,44 @@
 ﻿using System;
 using System.Configuration;
 using System.Data.SqlClient;
+using System.Threading.Tasks;
 
 namespace ADO.Net.Examples
 {
     class Program
     {
-        static void Main(string[] args)
+        static async Task Main(string[] args)
         {
             var connectionString = ConfigurationManager.ConnectionStrings["AzureDBConnection"].ConnectionString;
+            ExecuteScalar(connectionString);
+            await ExecuteScalarAsync(connectionString);
+        }
+
+        /// <summary>
+        /// The 'ExecuteScalar' method executes the query, and returns the first
+        /// column of the first row in the result set returned by the query (additional
+        /// columns or rows are ignored
+        /// </summary>
+        static void ExecuteScalar(string connectionString)
+        {
             using var conn = new SqlConnection(connectionString);
             using var cmd = new SqlCommand("SELECT COUNT(*) FROM production.products;", conn);
             conn.Open();
             var count = (int)cmd.ExecuteScalar();
+            Console.WriteLine(count);
+        }
+
+        /// <summary>
+        /// The 'ExecuteScalarAsync' method executes the query asynchronously, and returns the first
+        /// column of the first row in the result set returned by the query (additional
+        /// columns or rows are ignored
+        /// </summary>
+        static async Task ExecuteScalarAsync(string connectionString)
+        {
+            using var conn = new SqlConnection(connectionString);
+            using var cmd = new SqlCommand("SELECT COUNT(*) FROM production.stocks;", conn);
+            conn.Open();
+            var count = (int)(await cmd.ExecuteScalarAsync());
             Console.WriteLine(count);
         }
     }
