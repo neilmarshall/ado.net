af84ca45af80554538d90728080ebffe16fac3c8
Neil Marshall <Neil.Marshall@pfaroe.dev>
2020-11-12 13:55:14 +0000

    add example of querying multiple result sets

--- ADO.Net.Examples/Program.cs
+++ ADO.Net.Examples/Program.cs
@@ -17,6 +17,7 @@ namespace ADO.Net.Examples
                 ExecuteStoredProcedureWithOutputParameterAsync(connectionString));
             ExecuteReader(connectionString);
             ExecuteReaderWithTypeConversionAndNullChecking(connectionString);
+            ExecuteReaderWithMultipleResultSets(connectionString);
         }
 
         /// <summary>
@@ -97,5 +98,28 @@ namespace ADO.Net.Examples
             }
         }
 
+        /// <summary>
+        /// The 'NextResult' method on the DataReader class is used to iterate through
+        /// subsequent result sets
+        /// </summary>
+        static void ExecuteReaderWithMultipleResultSets(string connectionString)
+        {
+            using var conn = new SqlConnection(connectionString);
+            using var cmd = new SqlCommand("SELECT store_name FROM sales.stores; SELECT manager_id FROM sales.staffs;", conn);
+            conn.Open();
+            using var reader = cmd.ExecuteReader(CommandBehavior.CloseConnection);
+            while (reader.Read())
+            {
+                Console.WriteLine($"\tStore: {reader["store_name"]}");
+            }
+            reader.NextResult();
+            while (reader.Read())
+            {
+                if (!reader.IsDBNull(reader.GetOrdinal("manager_id")))
+                {
+                    Console.WriteLine($"\tManager ID: {(int)reader["manager_id"]}");
+                }
+            }
+        }
     }
 }
