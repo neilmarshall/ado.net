c55d60d457aa8366d93c35d526c2379677a80cd1
neilmarshall <neil.marshall@dunelm.org.uk>
2020-12-02 22:25:52 +0000

    add example of stored procedure returning result set

--- ADO.Net.Examples/Program.cs
+++ ADO.Net.Examples/Program.cs
@@ -18,7 +18,8 @@ namespace ADO.Net.Examples
             ExecuteScalar(connectionString);
             await Task.WhenAll(
                 ExecuteScalarAsync(connectionString),
-                ExecuteStoredProcedureWithOutputParameterAsync(connectionString));
+                ExecuteStoredProcedureWithOutputParameterAsync(connectionString),
+                ExecuteStoredProcedureReturningTable(connectionString));
             ExecuteReader(connectionString);
             ExecuteReaderWithTypeConversionAndNullChecking(connectionString);
             ExecuteReaderWithMultipleResultSets(connectionString);
@@ -87,6 +88,33 @@ namespace ADO.Net.Examples
                 conn.Open();
                 await cmd.ExecuteNonQueryAsync();
                 Console.WriteLine($"Total number of stores: {(int)cmd.Parameters["@count"].Value}");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
+
+        /// <summary>
+        /// We can also execute a stored procedure that does not have an output
+        /// parameter and instead simply returns a result set
+        /// </summary>
+        static async Task ExecuteStoredProcedureReturningTable(string connectionString)
+        {
+            try
+            {
+                using var conn = new SqlConnection(connectionString);
+                using var cmd = new SqlCommand("sales.get_employees_by_store", conn);
+                cmd.CommandType = CommandType.StoredProcedure;
+                cmd.Parameters.Add(new SqlParameter("@city", SqlDbType.VarChar, 255){ Value = "Rowlett" });
+                conn.Open();
+                using var reader = await cmd.ExecuteReaderAsync(CommandBehavior.CloseConnection);
+                while (reader.Read())
+                {
+                    Console.WriteLine($"\tFirst Name: {reader["first_name"]}");
+                    Console.WriteLine($"\tLast Name: {reader["last_name"]}");
+                    Console.WriteLine($"\tCity: {reader["city"]}");
+                }
             }
             catch (Exception ex)
             {
